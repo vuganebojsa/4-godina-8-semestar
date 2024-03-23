3fb67ae0cd446b7387ad0d7d3a503351c7b7316e
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-03-23 14:14:45 +0100

    changes

--- sistemi bazirani na znanju/Vežbe/Kostur/IME_PREZIME_BROJINDEKSA_GRUPA/sbnz-integracija-projekta/service/src/test/java/com/ftn/sbnz/service/tests/CEPConfigTest.java
+++ sistemi bazirani na znanju/Vežbe/Kostur/IME_PREZIME_BROJINDEKSA_GRUPA/sbnz-integracija-projekta/service/src/test/java/com/ftn/sbnz/service/tests/CEPConfigTest.java
@@ -26,9 +26,44 @@ public class CEPConfigTest {
         TransactionEvent t3 = new TransactionEvent(1L, 890.00, "Samsung");
         TransactionEvent t4 = new TransactionEvent(1L, 890.00, "Samsung");
         t1.setExecutionTime(new Date(clock.getCurrentTime()));
+
         t2.setExecutionTime(new Date(clock.getCurrentTime()));
         t3.setExecutionTime(new Date(clock.getCurrentTime()));
    
+        ksession.insert(t1);
+        ksession.insert(t2);
+        ksession.insert(t3);
+
+        ksession.fireAllRules();
+
+        clock.advanceTime(10, TimeUnit.MINUTES);
+        t4.setExecutionTime(new Date(clock.getCurrentTime()));
+        t4.setPhone("Tablet");
+        ksession.insert(t4);
+        ksession.fireAllRules();
+        //clock.advanceTime(2, TimeUnit.HOURS);
+      /**
+       * Ukoliko je isti kupac kupio bar 3 
+       * nova Samsung telefona u posljednjih 5 
+       * minuta čija je cena veća od 800e, 
+       * kreiraj kupon za 10% popusta
+       * na sledeću kupovinu tableta. Ukoliko kupi tablet, na cenu
+            se primenjuje kupon.
+       */
+    }
+    @Test
+    public void test2() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer(); 
+        KieSession ksession = kContainer.newKieSession("cepKsession");
+        SessionPseudoClock clock = ksession.getSessionClock();
+        TransactionEvent t1 = new TransactionEvent(1L, 850.00, "Samsung");
+        TransactionEvent t2 = new TransactionEvent(1L, 880.00, "Samsung");
+        TransactionEvent t3 = new TransactionEvent(1L, 890.00, "Samsung");
+        TransactionEvent t4 = new TransactionEvent(1L, 890.00, "Samsung");
+        t1.setExecutionTime(new Date(clock.getCurrentTime()));
+        t2.setExecutionTime(new Date(clock.getCurrentTime()));
+        t3.setExecutionTime(new Date(clock.getCurrentTime()));
         ksession.insert(t1);
         ksession.insert(t2);
         ksession.insert(t3);
@@ -36,7 +71,7 @@ public class CEPConfigTest {
 
         clock.advanceTime(10, TimeUnit.MINUTES);
         t4.setExecutionTime(new Date(clock.getCurrentTime()));
-        t4.setPhone("Tablet");
+        t4.setPhone("asdas");
         ksession.insert(t4);
         ksession.fireAllRules();
         //clock.advanceTime(2, TimeUnit.HOURS);
