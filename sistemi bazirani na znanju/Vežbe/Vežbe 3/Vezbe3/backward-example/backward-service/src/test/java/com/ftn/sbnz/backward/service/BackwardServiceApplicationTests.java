fe5fdfc7e8864e048e00d7dc070ea27620c53d37
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-03-23 22:27:40 +0100

    novoo

--- sistemi bazirani na znanju/Vežbe/Vežbe 3/Vezbe3/backward-example/backward-service/src/test/java/com/ftn/sbnz/backward/service/BackwardServiceApplicationTests.java
+++ sistemi bazirani na znanju/Vežbe/Vežbe 3/Vezbe3/backward-example/backward-service/src/test/java/com/ftn/sbnz/backward/service/BackwardServiceApplicationTests.java
@@ -1,13 +1,29 @@
 package com.ftn.sbnz.backward.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 
-@SpringBootTest
+import com.ftn.sbnz.backward.model.models.Location;
+
 class BackwardServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer(); 
+        KieSession ksession = kContainer.newKieSession();
+		ksession.insert(new Location("S", "Sa"));
+		ksession.insert(new Location("A", "Sa"));
+		ksession.insert(new Location("B", "Sa"));
+		ksession.insert(new Location("G S", "S"));
+		ksession.insert(new Location("G B", "S"));
+		ksession.insert(new Location("A B", "A"));
+		ksession.insert(new Location("A a a", "A B"));
+		ksession.insert(new Location("asfsa", "G B"));
+
+		ksession.fireAllRules();
 	}
 
 }
